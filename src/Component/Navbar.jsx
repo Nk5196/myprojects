3fcd44a4c89702bfe7c58630e93de1827e15a187
your github user name <narendrakandalkar@gmail.com>
2022-08-23 15:22:16 +0530

    afternoon

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,9 +1,17 @@
-import {Box} from "@chakra-ui/react"
+import {Box, Button,Flex, Spacer} from "@chakra-ui/react"
 
 function Navbar() {
     return (
         <Box>
-        Navbar
+            <Flex gap="10px" p="10px" borderBottom="1px" borderColor="gray.200">
+                <Button>PharmEasy</Button>
+                <Button>Express delivery</Button>
+                <Spacer></Spacer>
+                <Button>Download App</Button>
+                <Button>User</Button>
+                <Button>Offers</Button>
+                <Button>Cart</Button>
+            </Flex>
         </Box>
     )
 }
